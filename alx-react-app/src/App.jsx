903b99e4fc2be58193182d6a5cf7d504017f197a
Pachotek <palfredie@gmail.com>
2025-02-13 16:05:51 -0800

    Added new components in a single react application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,12 @@
-import WelcomeMessage from "./components/WelcomeMessage"
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 
 function App() {
   return(
     <>
-    <WelcomeMessage />
+   <Header />, <MainContent />,  <Footer />
     </>
   )
 }
