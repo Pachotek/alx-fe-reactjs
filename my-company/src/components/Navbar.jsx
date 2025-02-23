192443894c46415459bbf2bb2b17b8f60e983197
Pachotek <palfredie@gmail.com>
2025-02-23 03:50:48 -0800

    Modified Navbar.jsx to include inline styling-justifyContent

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav style={{ backgroundColor: 'gray', }}>
-        <ul style={{ display: 'flex', paddingLeft: '380px', gap: '20px', listStyle: 'none'}}>
+        <ul style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px', listStyle: 'none'}}>
             <li style={{ textDecoration: 'none'}}><Link style={{ color: 'white'}} to ="/">Home</Link></li>
             <li><Link style={{ color: 'white'}} to ="/about">About</Link></li>
             <li><Link style={{ color: 'white'}} to ="/services">Services</Link></li>
