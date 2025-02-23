9ceb15482f29326660bd4b5da97a1ab45e76cfbc
Pachotek <palfredie@gmail.com>
2025-02-23 03:44:16 -0800

    Modified Navbar.jsx to implement inline styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <nav>
-        <ul>
-            <li><Link to ="/">Home</Link></li>
-            <li><Link to ="/about">About</Link></li>
-            <li><Link to ="/services">Services</Link></li>
-            <li><Link to ="/contact">Contact</Link></li>
+    <nav style={{ backgroundColor: 'gray', }}>
+        <ul style={{ display: 'flex', paddingLeft: '380px', gap: '20px', listStyle: 'none'}}>
+            <li style={{ textDecoration: 'none'}}><Link style={{ color: 'white'}} to ="/">Home</Link></li>
+            <li><Link style={{ color: 'white'}} to ="/about">About</Link></li>
+            <li><Link style={{ color: 'white'}} to ="/services">Services</Link></li>
+            <li><Link style={{ color: 'white'}} to ="/contact">Contact</Link></li>
         </ul>
     </nav>
   )
